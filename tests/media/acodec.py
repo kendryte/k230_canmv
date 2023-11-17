c53369acc3ee254f155be0bb5997549d3ee156b0
gh-sxp <sunxiaopeng@canaan-creative.com>
2023-11-17 15:03:27 +0800

    Add code comments

--- tests/media/acodec.py
+++ tests/media/acodec.py
@@ -5,25 +5,26 @@
 # You can collect raw data and encode it into g711 or decode it into raw data output.
 
 
-from media.pyaudio import *
-from media.media import *
-import media.g711 as g711
-from mpp.payload_struct import *
+from media.pyaudio import * #导入pyaudio模块，用于采集和播放音频
+from media.media import * #导入media模块，用于初始化vb buffer
+import media.g711 as g711 #导入g711模块，用于g711编解码
+from mpp.payload_struct import * #导入payload模块，用于获取音视频编解码类型
 
 def encode_audio(filename, duration):
-    CHUNK = int(44100/25)
-    FORMAT = paInt16
-    CHANNELS = 2
-    RATE = 44100
+    CHUNK = int(44100/25) #设置音频chunk值
+    FORMAT = paInt16 #设置采样精度
+    CHANNELS = 2 #设置声道数
+    RATE = 44100 #设置采样率
 
     p = PyAudio()
-    p.initialize(CHUNK)
-    enc = g711.Encoder(K_PT_G711A,CHUNK)
-    ret = media.buffer_init()
+    p.initialize(CHUNK) #初始化PyAudio对象
+    enc = g711.Encoder(K_PT_G711A,CHUNK) #创建g711编码器对象
+    ret = media.buffer_init() #vb buffer初始化
     if ret:
         print("record_audio, buffer_init failed")
 
-    enc.create()
+    enc.create() #创建编码器
+    #创建音频输入流
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
@@ -31,109 +32,113 @@ def encode_audio(filename, duration):
                     frames_per_buffer=CHUNK)
 
     frames = []
-
+    #采集音频数据编码并存入列表
     for i in range(0, int(RATE / CHUNK * duration)):
-        frame_data = stream.read()
-        data = enc.encode(frame_data)
-        frames.append(data)
+        frame_data = stream.read() #从音频输入流中读取音频数据
+        data = enc.encode(frame_data) #编码音频数据为g711
+        frames.append(data)  #将g711编码数据保存到列表中
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    enc.destroy()
+    stream.stop_stream() #停止音频输入流
+    stream.close() #关闭音频输入流
+    p.terminate() #释放音频对象
+    enc.destroy() #销毁g711音频编码器
 
+    #将g711编码数据存入文件中
     with open(filename,mode='w') as wf:
         wf.write(b''.join(frames))
 
-    media.buffer_deinit()
+    media.buffer_deinit() #释放vb buffer
 
 def decode_audio(filename):
-    #读取音频文件
-    wf = open(filename,mode='rb')
-    FORMAT = paInt16
-    CHANNELS = 2
-    RATE = 44100
-    CHUNK = int(RATE/25)
+    wf = open(filename,mode='rb') #打开g711文件
+    FORMAT = paInt16 #设置音频chunk值
+    CHANNELS = 2 #设置声道数
+    RATE = 44100 #设置采样率
+    CHUNK = int(RATE/25) #设置音频chunk值
 
-    #播放音频操作
     p = PyAudio()
-    p.initialize(CHUNK)
-    dec = g711.Decoder(K_PT_G711A,CHUNK)
-    ret = media.buffer_init()
+    p.initialize(CHUNK) #初始化PyAudio对象
+    dec = g711.Decoder(K_PT_G711A,CHUNK) #创建g711解码器对象
+    ret = media.buffer_init() #vb buffer初始化
     if ret:
         print("play_audio, buffer_init failed")
 
-    dec.create()
+    dec.create() #创建解码器
 
+    #创建音频输出流
     stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 output=True,
                 frames_per_buffer=CHUNK)
 
-    stream_len = CHUNK*CHANNELS*2//2
-    stream_data = wf.read(stream_len)
+    stream_len = CHUNK*CHANNELS*2//2  #设置每次读取的g711数据流长度
+    stream_data = wf.read(stream_len) #从g711文件中读取数据
 
+    #解码g711文件并播放
     while stream_data:
-        frame_data = dec.decode(stream_data)
-        stream.write(frame_data)
-        stream_data = wf.read(stream_len)
+        frame_data = dec.decode(stream_data) #解码g711文件
+        stream.write(frame_data) #播放raw数据
+        stream_data = wf.read(stream_len) #从g711文件中读取数据
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    dec.destroy()
-    wf.close()
+    stream.stop_stream() #停止音频输入流
+    stream.close() #关闭音频输入流
+    p.terminate() #释放音频对象
+    dec.destroy() #销毁解码器
+    wf.close() #关闭g711文件
 
-    media.buffer_deinit()
+    media.buffer_deinit() #释放vb buffer
 
 def loop_codec(duration):
-    CHUNK = int(44100/25)
-    FORMAT = paInt16
-    CHANNELS = 2
-    RATE = 44100
+    CHUNK = int(44100/25) #设置音频chunk值
+    FORMAT = paInt16 #设置采样精度
+    CHANNELS = 2 #设置声道数
+    RATE = 44100 #设置采样率
 
     p = PyAudio()
-    p.initialize(CHUNK)
-    dec = g711.Decoder(K_PT_G711A,CHUNK)
-    enc = g711.Encoder(K_PT_G711A,CHUNK)
-    ret = media.buffer_init()
+    p.initialize(CHUNK) #初始化PyAudio对象
+    dec = g711.Decoder(K_PT_G711A,CHUNK) #创建g711解码器对象
+    enc = g711.Encoder(K_PT_G711A,CHUNK) #创建g711编码器对象
+    ret = media.buffer_init() #vb buffer初始化
     if ret:
         print("loop_audio, buffer_init failed")
 
-    dec.create()
-    enc.create()
+    dec.create() #创建g711解码器
+    enc.create() #创建g711编码器
 
+    #创建音频输入流
     input_stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
 
+    #创建音频输出流
     output_stream = p.open(format=FORMAT,
                      channels=CHANNELS,
                      rate=RATE,
                      output=True,
                      frames_per_buffer=CHUNK)
 
+    #从音频输入流中获取数据->编码->解码->写入到音频输出流中
     for i in range(0, int(RATE / CHUNK * duration)):
-        frame_data = input_stream.read()
-        stream_data = enc.encode(frame_data)
-        frame_data = dec.decode(stream_data)
-        output_stream.write(frame_data)
+        frame_data = input_stream.read() #从音频输入流中获取raw音频数据
+        stream_data = enc.encode(frame_data) #编码音频数据为g711
+        frame_data = dec.decode(stream_data) #解码g711数据为raw数据
+        output_stream.write(frame_data) #播放raw数据
 
 
-    input_stream.stop_stream()
-    output_stream.stop_stream()
-    input_stream.close()
-    output_stream.close()
-    p.terminate()
-    dec.destroy()
-    enc.destroy()
+    input_stream.stop_stream() #停止音频输入流
+    output_stream.stop_stream() #停止音频输出流
+    input_stream.close() #关闭音频输入流
+    output_stream.close() #关闭音频输出流
+    p.terminate() #释放音频对象
+    dec.destroy() #销毁g711解码器
+    enc.destroy() #销毁g711编码器
 
-    media.buffer_deinit()
+    media.buffer_deinit() #释放vb buffer
 
-#encode_audio('/sdcard/app/test.g711a', 15)
-#decode_audio('/sdcard/app/test.g711a')
-loop_codec(15)
+#encode_audio('/sdcard/app/test.g711a', 15) #采集并编码g711文件
+#decode_audio('/sdcard/app/test.g711a') #解码g711文件并输出
+loop_codec(15) #采集音频数据->编码g711->解码g711->播放音频
 print("audio codec sample done")
--- tests/media/audio.py
+++ tests/media/audio.py
@@ -4,22 +4,23 @@
 #
 # You can play wav files or capture audio to save as wav
 
-from media.pyaudio import *
-import media.wave as wave
-from media.media import *
+from media.pyaudio import * #导入pyaudio模块，用于采集和播放音频
+import media.wave as wave   #导入wav模块，用于保存和加载wav音频文件
+from media.media import *   #导入media模块，用于初始化vb buffer
 
 def record_audio(filename, duration):
-    CHUNK = int(44100/25)
-    FORMAT = paInt16
-    CHANNELS = 2
-    RATE = 44100
+    CHUNK = int(44100/25)  #设置音频chunk值
+    FORMAT = paInt16       #设置采样精度
+    CHANNELS = 2           #设置声道数
+    RATE = 44100           #设置采样率
 
     p = PyAudio()
-    p.initialize(CHUNK)
-    ret = media.buffer_init()
+    p.initialize(CHUNK)    #初始化PyAudio对象
+    ret = media.buffer_init() #vb buffer初始化
     if ret:
         print("record_audio, buffer_init failed")
 
+    #创建音频输入流
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
@@ -27,90 +28,96 @@ def record_audio(filename, duration):
                     frames_per_buffer=CHUNK)
 
     frames = []
-
+    #采集音频数据并存入列表
     for i in range(0, int(RATE / CHUNK * duration)):
         data = stream.read()
         frames.append(data)
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
 
-    wf = wave.open(filename, 'wb')
-    wf.set_channels(CHANNELS)
-    wf.set_sampwidth(p.get_sample_size(FORMAT))
-    wf.set_framerate(RATE)
-    wf.write_frames(b''.join(frames))
-    wf.close()
+    stream.stop_stream() #停止采集音频数据
+    stream.close()#关闭音频输入流
+    p.terminate()#释放音频对象
 
-    media.buffer_deinit()
+    #将列表中的数据保存到wav文件中
+    wf = wave.open(filename, 'wb') #创建wav 文件
+    wf.set_channels(CHANNELS) #设置wav 声道数
+    wf.set_sampwidth(p.get_sample_size(FORMAT))  #设置wav 采样精度
+    wf.set_framerate(RATE)  #设置wav 采样率
+    wf.write_frames(b''.join(frames)) #存储wav音频数据
+    wf.close() #关闭wav文件
+
+    media.buffer_deinit() #释放vb buffer
 
 def play_audio(filename):
-    #读取音频文件
-    wf = wave.open(filename, 'rb')
-    CHUNK = int(wf.get_framerate()/25)
 
-    #播放音频操作
+    wf = wave.open(filename, 'rb')#打开wav文件
+    CHUNK = int(wf.get_framerate()/25)#设置音频chunk值
+
     p = PyAudio()
-    p.initialize(CHUNK)
-    ret = media.buffer_init()
+    p.initialize(CHUNK) #初始化PyAudio对象
+    ret = media.buffer_init()#vb buffer初始化
     if ret:
         print("play_audio, buffer_init failed")
 
+    #创建音频输出流，设置的音频参数均为wave中获取到的参数
     stream = p.open(format=p.get_format_from_width(wf.get_sampwidth()),
                 channels=wf.get_channels(),
                 rate=wf.get_framerate(),
                 output=True,frames_per_buffer=CHUNK)
-    data = wf.read_frames(CHUNK)
+
+    data = wf.read_frames(CHUNK)#从wav文件中读取数一帧数据
 
     while data:
-        stream.write(data)
-        data = wf.read_frames(CHUNK)
+        stream.write(data)  #将帧数据写入到音频输出流中
+        data = wf.read_frames(CHUNK) #从wav文件中读取数一帧数据
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    wf.close()
+    stream.stop_stream() #停止音频输出流
+    stream.close()#关闭音频输出流
+    p.terminate()#释放音频对象
+    wf.close()#关闭wav文件
 
-    media.buffer_deinit()
+    media.buffer_deinit()#释放vb buffer
 
 
 def loop_audio(duration):
-    CHUNK = int(44100/25)
-    FORMAT = paInt16
-    CHANNELS = 2
-    RATE = 44100
+    CHUNK = int(44100/25)#设置音频chunck
+    FORMAT = paInt16 #设置音频采样精度
+    CHANNELS = 2 #设置音频声道数
+    RATE = 44100 #设置音频采样率
 
     p = PyAudio()
-    p.initialize(CHUNK)
-    ret = media.buffer_init()
+    p.initialize(CHUNK)#初始化PyAudio对象
+    ret = media.buffer_init() #初始化vb buffer
     if ret:
         print("loop_audio, buffer_init failed")
 
+    #创建音频输入流
     input_stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
 
+    #创建音频输出流
     output_stream = p.open(format=FORMAT,
                      channels=CHANNELS,
                      rate=RATE,
                      output=True,frames_per_buffer=CHUNK)
 
+    #从音频输入流中获取数据写入到音频输出流中
     for i in range(0, int(RATE / CHUNK * duration)):
         output_stream.write(input_stream.read())
 
 
-    input_stream.stop_stream()
-    output_stream.stop_stream()
-    input_stream.close()
-    output_stream.close()
-    p.terminate()
+    input_stream.stop_stream()#停止音频输入流
+    output_stream.stop_stream()#停止音频输出流
+    input_stream.close() #关闭音频输入流
+    output_stream.close() #关闭音频输出流
+    p.terminate() #释放音频对象
 
-    media.buffer_deinit()
+    media.buffer_deinit() #释放vb buffer
 
-#play_audio('/sdcard/app/input.wav')
-#record_audio('/sdcard/app/output.wav', 15)
-loop_audio(15)
+#play_audio('/sdcard/app/input.wav') #播放wav文件
+#record_audio('/sdcard/app/output.wav', 15)  #录制wav文件
+loop_audio(15) #采集音频并输出
 print("audio sample done")
